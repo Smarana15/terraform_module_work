541a1a8be00d8e61cd217338c438eb6ef9e24993
Smarana15 <smaranam1593@gmail.com>
2025-07-03 23:06:07 +0530

    Create main.tf

---
+++ web/main.tf
@@ -0,0 +1,15 @@
+module "web" {
+ source = "../basicinfra/modules"
+ cidr_block = "${var.cidr_block}"
+ tag = "${var.tag}"
+ subnet1_cidr_block = "${var.subnet1_cidr_block}"
+ availability_zone = "${var.availability_zone }"
+ subnet2_cidr_block = "${var.subnet2_cidr_block}"
+ terraform-key-pair = "${var.terraform-key-pair}"
+ ami = "${var.ami}"
+ instance_type = "${var.instance_type}"
+ availability_zone_1 = "${var.availability_zone_1}"
+ autoscaling_group_min_size = "${var.autoscaling_group_min_size}"
+ autoscaling_group_max_size = "${var.autoscaling_group_max_size}"
+ }
+ 
