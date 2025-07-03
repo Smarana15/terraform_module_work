ede554786b4633e2b8d5c9549a88c5c5a03d9c38
Smarana15 <smaranam1593@gmail.com>
2025-07-03 23:17:16 +0530

    Create main.tf

---
+++ datasource/main.tf
@@ -0,0 +1,17 @@
+data "aws_vpc" "vpc" {
+    filter {
+      name = "tag:Name"
+      values = ["dev"]
+    }
+}
+resource "aws_subnet" "public_subnets" {
+vpc_id = data.aws_vpc.vpc.id
+cidr_block = "10.0.5.0/24"
+tags = {
+   Name = "publicdata"
+}
+}
+data "aws_ami" "latest" {
+   most-recent = true
+   owners = ["068977*****"]
+}
